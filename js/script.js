361195f0cc4ff91f926479db9dff8a71e9f40ccd
kalet <kaleth164@gmail.com>
2025-01-05 16:48:57 -0500

    mejora de la slidebar y agregue info. en el about me

--- js/script.js
+++ js/script.js
@@ -1,11 +1,14 @@
 window.addEventListener('scroll', function() {
     const header = document.querySelector('header');
     const sidebar = document.querySelector('.sidebar');
+    const mainContent = document.querySelector('.main-content'); // Añade esta línea
     const headerBottom = header.getBoundingClientRect().bottom;
 
     if (headerBottom <= 0) {
         sidebar.style.display = 'block';
+        mainContent.classList.add('with-sidebar'); // Añade esta línea
     } else {
         sidebar.style.display = 'none';
+        mainContent.classList.remove('with-sidebar'); // Añade esta línea
     }
 });
